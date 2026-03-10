21eea177592e1d5dd3f600525430f8d994b1b737
BabuLahade <babulahade@gmail.com>
2026-03-10 12:07:58 +0530

    infrastructure modified

--- infrastructure/terraform/modules/igw-natgw/outputs.tf
+++ infrastructure/terraform/modules/igw-natgw/outputs.tf
@@ -1,3 +1,9 @@
 output "nat_public_ips" {
   value = aws_nat_gateway.natgw[*].public_ip
 }
+output "igw_id" {
+  value = aws_internet_gateway.igw.id
+}
+output "natgw_ids" {
+  value = aws_nat_gateway.natgw[*].id
+}
