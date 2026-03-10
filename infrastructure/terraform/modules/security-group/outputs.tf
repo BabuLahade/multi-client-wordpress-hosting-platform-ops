35a002db709fa7c7129a03e0f1ba135ee2c6b32d
BabuLahade <babulahade@gmail.com>
2026-03-10 13:05:01 +0530

    infrastructure updated

---
+++ infrastructure/terraform/modules/security-group/outputs.tf
@@ -0,0 +1,13 @@
+output "security_group_ids" {
+    value = aws_security_group.sg[*].id
+  
+}
+output "alb_security_group_id" {
+    value = aws_security_group.alb_sg.id
+}
+output "app_security_group_id" {
+    value =aws_security_group.app.id
+}
+output "db_security_group_id" {
+    value = aws_security_group.db.id
+}
