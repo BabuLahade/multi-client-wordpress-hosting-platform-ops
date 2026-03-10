7394d7b756575f5d80c3c245c6d49f1d45e4cfb9
BabuLahade <babulahade@gmail.com>
2026-03-10 11:19:33 +0530

    infra updated

---
+++ infrastructure/terraform/modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable  "project_name" {
+    description = "The name of project here declared"
+    type= string
+}
+
+variable "vpc_cidr" {
+    description = "cidr ip range for VPC"
+    type = string 
+}
