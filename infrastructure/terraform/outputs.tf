c9e2eb3ed6e0b1e30c61eee56ef651e8a64b814e
BabuLahade <babulahade@gmail.com>
2026-03-10 14:39:54 +0530

    updated

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -35,9 +35,9 @@
 #   ])
 # }
 
-output "public_ip" {
-    value = module.ec2.public_ip
-}
-output "vpc_id" {
-    value = module.vpc.vpc_id
-}
+# output "public_ip" {
+#     value = module.ec2.public_ip
+# }
+# output "vpc_id" {
+#     value = module.vpc.vpc_id
+# }
