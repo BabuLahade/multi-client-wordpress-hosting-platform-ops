d1282b6e45ecc19f46a5a9a7a5efbd22f3aa6bcd
BabuLahade <babulahade@gmail.com>
2026-03-10 12:05:51 +0530

    infrastructure as a code modified

---
+++ infrastructure/terraform/modules/igw-natgw/main.tf
@@ -0,0 +1,23 @@
+resource "aws_internet_gateway" "igw" {
+    vpc_id = var.vpc_id
+    tags = {
+        Name = "${var.project_name}-igw"
+    }
+}
+
+resource "aws_eip" "natgw_eip" {
+    count = length(var.private_app_subnet_ids)
+    domain = "vpc"
+    tags = {
+        Name = "${var.project_name}-natgw-eip-${count.index + 1}"
+    }
+}
+
+resource "aws_nat_gateway" "natgw" {
+    count =length(var.public_subnet_ids)
+    subnet_id = var.public_subnet_ids[count.index]
+    allocation_id = var.aws_eip.natgw_eip[count.index].id
+    tags = {
+        Name = "${var.project_name}-natgw-${count.index +1}"
+    }
+}
