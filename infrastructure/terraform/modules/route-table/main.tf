2943c1946be096628b6fae3d6b20c832a45525eb
BabuLahade <babulahade@gmail.com>
2026-03-10 12:28:46 +0530

    infrastructure updated

---
+++ infrastructure/terraform/modules/route-table/main.tf
@@ -0,0 +1,37 @@
+resource "aws_route_table" "public_rt" {
+    vpc_id = var.vpc_id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = var.igw_id
+    }
+    tags = {
+        Name = "${var.project_name}-public-rt"
+    }
+}
+
+resource "aws_route_table" "private_rt" {
+    vpc_id = var.vpc_id
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = var.natgw_ids[0]
+    }
+    route {
+        cidr_block = "0.0.0.0/0"
+        nat_gateway_id = var.natgw_ids[1]
+    }
+    tags = {
+        Name = "${var.project_name}-private-rt"
+    }
+}
+
+resource "aws_route_table_association" "public_rt_assoc" {
+    count = length(var.public_subnet_ids)
+    subnet_id = var.public_subnet_ids[count.index]
+    route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "private_rt_assoc" {
+    count = length(var.private_app_subnet_ids)
+    subnet_id = var.private_app_subnet_ids[count.index]
+    route_table_id = aws_route_table.private_rt.id
+}   
