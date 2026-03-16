14a87e5ae19746da1adbd9473229cd55b87d7c4d
BabuLahade <babulahade@gmail.com>
2026-03-16 10:43:33 +0530

    infra updated

--- infrastructure/terraform/modules/ALB/main.tf
+++ infrastructure/terraform/modules/ALB/main.tf
@@ -53,3 +53,43 @@
 #     }
   
 # }
+
+
+resource "aws_lb" "alb" {
+    name = "${var.project_name}-alb"
+    load_balancer_type = application
+    internal = false
+    subnets = var.public_subnet_ids
+    security_groups = [var.alb_security_group_id]
+    tags = {
+        Name = "${var.project_name}-alb"
+    }
+}
+
+resource "aws_lb_target_group" "alb_tg" {
+    name = "${var.project_name}-alb-tg"
+    port = 80
+    protocol = "HTTP"
+
+    vpc_id = var.vpc_id
+    health_check {
+        path = "/"
+        protocol = "HTTP"
+        interval = 30
+        timeout = 5
+        healthy_threshold = 5
+        unhealthy_threshold = 2
+
+    }
+}
+
+resource "aws_lb_listener" "alb_listener" {
+    load_balancer_arn = aws_lb.alb.arn
+    port = 80
+    protocol = "HTTP"
+
+    default_action {
+      type = "forward"
+      target_group_arn = aws_lb_target_group.alb_tg.arn
+    }
+}
