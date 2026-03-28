e5a20cc250815b8efafa9637f9d9470270f442e8
BabuLahade <babulahade@gmail.com>
2026-03-28 17:11:12 +0530

    updated stack

--- infrastructure/terraform/modules/ALB/main.tf
+++ infrastructure/terraform/modules/ALB/main.tf
@@ -107,6 +107,17 @@ resource "aws_lb_listener" "alb_listener" {
 
     default_action {
       type = "forward"
-      target_group_arn = aws_lb_target_group.alb_tg.arn
+      target_group_arn = aws_lb_target_group.alb_tg_1.arn
+    }
+}
+
+resource "aws_lb_listener" "alb_listener_1"{
+    load_balancer_arn =aws_lb.alb.arn
+    port = 80
+    protocol = "HTTP"
+
+    default_action {
+      type ="forward"
+      target_group_arn = aws_lb_target_group.alb_tg_2.arn
     }
 }
