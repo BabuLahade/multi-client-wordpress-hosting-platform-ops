2e005f1e80ed87b3f79a554da3e7e04ac8c045e9
BabuLahade <babulahade@gmail.com>
2026-03-31 23:14:30 +0530

    in infrastructure ecs added

--- infrastructure/terraform/modules/ec2_launch_template/user_data.sh
+++ infrastructure/terraform/modules/ec2_launch_template/user_data.sh
@@ -93,12 +93,12 @@ until docker exec wordpress-app ls /var/www/html >/dev/null 2>&1; do
 done
 
 # update wp-config
+# docker exec wordpress-app sh -c "cat >> /var/www/html/wp-config.php <<'EOL'
+
+# # if (isset(\$_SERVER['HTTP_X_FORWARDED_PROTO']) && \$_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+# #     \$_SERVER['HTTPS'] = 'on';
+# # }
 docker exec wordpress-app sh -c "cat >> /var/www/html/wp-config.php <<'EOL'
-
-# if (isset(\$_SERVER['HTTP_X_FORWARDED_PROTO']) && \$_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-#     \$_SERVER['HTTPS'] = 'on';
-# }
-
 define('WP_HOME', 'http://${name}');
 define('WP_SITEURL', 'http://${name}');
 
