a6aab5fa0fc14ecdac646bb66db718d907006c94
BabuLahade <babulahade@gmail.com>
2026-04-01 17:07:29 +0530

    in infrastructure ecs added

--- infrastructure/terraform/modules/IAM/main.tf
+++ infrastructure/terraform/modules/IAM/main.tf
@@ -48,3 +48,16 @@ resource "aws_iam_role_policy_attachment" "ec2_ssm_policy_attachment" {
 #     name = "${var.project_name}-ec2-ssm-instance-profile"
 #     role = aws_iam_role.ec2_ssm_role.name
 # }
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+        Action = "sts-AssumeRole"
+        Effect = "Allow"
+        Principal = {
+            Service = "ecs-tasks.amazonaws.com"
+        }
+    }]
+  })
+}
