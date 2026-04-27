f75d29410d0afcc01e4dd331c6b903a1ccea2595
BabuLahade <babulahade@gmail.com>
2026-04-27 17:46:26 +0530

    files updated

--- infrastructure/terraform/providers.tf
+++ infrastructure/terraform/providers.tf
@@ -7,13 +7,12 @@ terraform {
     }
   }
 }
-# This is your normal provider keeping your servers in Europe
+
 provider "aws" {
   region = "eu-north-1"
 }
 
-# Add this right below it! 
-# The "alias" tells Terraform this is a secondary, special-use provider.
+
 provider "aws" {
   alias  = "us_east_1"
   region = "us-east-1"
