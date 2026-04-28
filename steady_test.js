45efc91ed5ec14fbf90b032922cffc7e629eaa5d
BabuLahade <babulahade@gmail.com>
2026-04-28 11:35:09 +0530

    staedy test

--- steady_test.js
+++ steady_test.js
@@ -4,18 +4,17 @@ import { check, sleep } from 'k6';
 // The Test Requirements: 15 minutes total
 export const options = {
   stages: [
-    { duration: '2m', target: 200 },   // Ramp up to 200 virtual users over 2 mins
-    { duration: '12m', target: 200 },  // Hold steady for 12 minutes (The Soak)
-    { duration: '1m', target: 0 },     // Ramp down safely over 1 min
+    { duration: '2m', target: 200 },   
+    { duration: '12m', target: 200 },  
+    { duration: '1m', target: 0 },     
   ],
 };
 
 export default function () {
-  // ⚠️ REQUIREMENT: Replace with your actual ALB URL from the AWS Console!
-  // It should look something like: http://internal-wordpress-alb-12345.eu-north-1.elb.amazonaws.com
-  const albUrl = 'http://wordpress-hosting-alb-272004923.eu-north-1.elb.amazonaws.com';
+ 
+  const albUrl = 'http://wordpress-hosting-alb-67884335.eu-north-1.elb.amazonaws.com';
 
-  // Array of your clients
+  
   const clients = [
     'client3.babu-lahade.online',
     'client4.babu-lahade.online',
