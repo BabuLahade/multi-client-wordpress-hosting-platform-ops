38dbe63b54839b0b8a5e49f622aa28b2e72fcf68
BabuLahade <babulahade@gmail.com>
2026-05-10 18:26:11 +0530

    testing started

--- load_testing.js
+++ load_testing.js
@@ -3,8 +3,8 @@ import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '2m', target: 1100 }, 
-    { duration: '3m', target: 1100 }, 
+    { duration: '2m', target: 1000 }, 
+    { duration: '3m', target: 1000 }, 
     { duration: '1m', target: 0 },    
   ],
 };
